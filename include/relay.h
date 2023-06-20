06f2df78823aff3551bdf5a93ef9c8ce612283b9
alirezainallo <alirezainallo@gmail.com>
2023-06-21 01:22:58 +0330

    write relay module

---
+++ include/relay.h
@@ -0,0 +1,45 @@
+#ifndef _RELAY_H
+#define _RELAY_H
+
+#include "main.h"
+
+typedef enum{
+    RELAY_FAN = 0,
+    RELAY_FERTILIZING,
+    RELAY_HEATER,
+    RELAY_LIGHT,
+    RELAY_WATERING,
+}relay_t;
+
+#define RELAY_WATERING_DIR  DDRA
+#define RELAY_WATERING_PORT PORTA
+#define RELAY_WATERING_PIN  PINA
+#define RELAY_WATERING_Pin  PA0
+
+#define RELAY_FERTILIZING_DIR  DDRA
+#define RELAY_FERTILIZING_PORT PORTA
+#define RELAY_FERTILIZING_PIN  PINA
+#define RELAY_FERTILIZING_Pin  PA1
+
+#define RELAY_FAN_DIR  DDRA
+#define RELAY_FAN_PORT PORTA
+#define RELAY_FAN_PIN  PINA
+#define RELAY_FAN_Pin  PA2
+
+#define RELAY_HEATER_DIR  DDRA
+#define RELAY_HEATER_PORT PORTA
+#define RELAY_HEATER_PIN  PINA
+#define RELAY_HEATER_Pin  PA3
+
+#define RELAY_LIGHT_DIR  DDRA
+#define RELAY_LIGHT_PORT PORTA
+#define RELAY_LIGHT_PIN  PINA
+#define RELAY_LIGHT_Pin  PA6
+
+
+
+void relay_init(void);
+void turnOff(relay_t relay);
+void turnOn(relay_t relay);
+
+#endif //_RELAY_H
---
+++ src/relay.c
@@ -0,0 +1,77 @@
+#include "relay.h"
+
+
+typedef enum{
+	PIN_LOW_LEVEL  = 0,
+	PIN_HIGH_LEVEL = 1,
+}PinLevel;
+
+void writePin(uint8_t *reg, uint8_t pin, PinLevel level){
+    if(level){
+        *reg |=  (1 << pin);
+    }
+    else{
+        *reg &= ~(1 << pin);
+    }
+}
+
+void relay_init(void){
+    //set as output
+    writePin(&RELAY_FAN_DIR, RELAY_FAN_Pin, PIN_HIGH_LEVEL);
+    writePin(&RELAY_FERTILIZING_DIR, RELAY_FERTILIZING_Pin, PIN_HIGH_LEVEL);
+    writePin(&RELAY_HEATER_DIR, RELAY_HEATER_Pin, PIN_HIGH_LEVEL);
+    writePin(&RELAY_LIGHT_DIR, RELAY_LIGHT_Pin, PIN_HIGH_LEVEL);
+    writePin(&RELAY_WATERING_DIR, RELAY_WATERING_Pin, PIN_HIGH_LEVEL);
+    
+    writePin(&RELAY_FAN_PORT, RELAY_FAN_Pin, PIN_LOW_LEVEL);
+    writePin(&RELAY_FERTILIZING_PORT, RELAY_FERTILIZING_Pin, PIN_LOW_LEVEL);
+    writePin(&RELAY_HEATER_PORT, RELAY_HEATER_Pin, PIN_LOW_LEVEL);
+    writePin(&RELAY_LIGHT_PORT, RELAY_LIGHT_Pin, PIN_LOW_LEVEL);
+    writePin(&RELAY_WATERING_PORT, RELAY_WATERING_Pin, PIN_LOW_LEVEL); 
+}
+void turnOff(relay_t relay){
+    switch (relay)
+    {
+        case RELAY_FAN:
+            writePin(&RELAY_FAN_PORT, RELAY_FAN_Pin, PIN_LOW_LEVEL);
+            break;
+        case RELAY_FERTILIZING:
+            writePin(&RELAY_FERTILIZING_PORT, RELAY_FERTILIZING_Pin, PIN_LOW_LEVEL);
+            break;
+        case RELAY_HEATER:
+            writePin(&RELAY_HEATER_PORT, RELAY_HEATER_Pin, PIN_LOW_LEVEL);
+            break;
+        case RELAY_LIGHT:
+            writePin(&RELAY_LIGHT_PORT, RELAY_LIGHT_Pin, PIN_LOW_LEVEL);
+            break;
+        case RELAY_WATERING:
+            writePin(&RELAY_WATERING_PORT, RELAY_WATERING_Pin, PIN_LOW_LEVEL);
+            break;
+        
+        default:
+            break;
+    }
+}
+void turnOn(relay_t relay){
+    switch (relay)
+    {
+        case RELAY_FAN:
+            writePin(&RELAY_FAN_PORT, RELAY_FAN_Pin, PIN_HIGH_LEVEL);
+            break;
+        case RELAY_FERTILIZING:
+            writePin(&RELAY_FERTILIZING_PORT, RELAY_FERTILIZING_Pin, PIN_HIGH_LEVEL);
+            break;
+        case RELAY_HEATER:
+            writePin(&RELAY_HEATER_PORT, RELAY_HEATER_Pin, PIN_HIGH_LEVEL);
+            break;
+        case RELAY_LIGHT:
+            writePin(&RELAY_LIGHT_PORT, RELAY_LIGHT_Pin, PIN_HIGH_LEVEL);
+            break;
+        case RELAY_WATERING:
+            writePin(&RELAY_WATERING_PORT, RELAY_WATERING_Pin, PIN_HIGH_LEVEL);
+            break;
+        
+        default:
+            break;
+    }
+}
